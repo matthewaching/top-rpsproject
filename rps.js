03dd14ea222bba6a05fcf829eb47dcb1db0ba9b2
Matthew Ching <matthewaching@gmail.com>
2022-07-05 14:02:04 -0500

    Add five round game loop with final scoreboard.

--- rps.js
+++ rps.js
@@ -1,12 +1,28 @@
+// Establishing main variables.
+
 let computerSelection;
-
 let playerSelection;
+let playerAnswer;
+let gameResult;
+let playerScore = 0;
+let computerScore = 0;
 
-let playerAnswer = prompt('Choose your weapon of choice! Rock, paper, or scissors?');
+for (let i = 0; i < 5; i++) {
+    playerAnswer = prompt('Choose your weapon of choice! Rock, paper, or scissors?');
+    playerSelection = rejectAnswer(playerAnswer);
+    computerPlay();
+    gameResult = playRound(playerSelection, computerSelection);
+    if (gameResult === 'pwin') {
+        playerScore++;
+    } else if (gameResult === 'cwin') {
+        computerScore++;
+    }
+    console.log(computerSelection);
+}
 
-playerSelection = rejectAnswer(playerAnswer);
-console.log(computerPlay());
-console.log(playRound(playerSelection, computerSelection));
+// Final results. Plan to add victor declaration.
+
+alert('Final results: You scored ' + playerScore + '. Computer scored ' + computerScore + '.');
 
 function computerPlay() {
     const choiceNumber = Math.floor(Math.random() * 3);
@@ -30,6 +46,8 @@ function rejectAnswer(answer) {
     return answer.toLowerCase();
     }
 }
+
+// Plan to add computerSelection into alerts so that player is aware. 
 
 function playRound (playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {

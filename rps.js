9021303b71eec1512b67d68bc2fab0006f94b0e8
Matthew Ching <matthewaching@gmail.com>
2022-07-05 13:47:03 -0500

    Add playRound function which simulates single round of RPS

--- rps.js
+++ rps.js
@@ -1,5 +1,12 @@
 let computerSelection;
+
 let playerSelection;
+
+let playerAnswer = prompt('Choose your weapon of choice! Rock, paper, or scissors?');
+
+playerSelection = rejectAnswer(playerAnswer);
+console.log(computerPlay());
+console.log(playRound(playerSelection, computerSelection));
 
 function computerPlay() {
     const choiceNumber = Math.floor(Math.random() * 3);
@@ -13,21 +20,44 @@ function computerPlay() {
     return computerSelection;
 }
 
-let playerAnswer = prompt('Choose your weapon of choice! Rock, paper, or scissors?');
-
 function rejectAnswer(answer) {
     if (!(answer.toLowerCase() === 'rock' || answer.toLowerCase() === 'paper' || answer.toLowerCase() === 'scissors')) {
         alert('That\'s not one of your options! Please enter a valid choice.');
         answer = prompt('Choose your weapon of choice! Rock, paper, or scissors?');
         answer = rejectAnswer(answer);
-        return answer;
+        return answer.toLowerCase();
     } else {
-    return answer;
+    return answer.toLowerCase();
     }
 }
 
-// playerAnswer = rejectAnswer(playerAnswer);
-
-// function playRound (playerSelection, computerSelection) {
-
-// }
+function playRound (playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        alert('Player and computer tie!');
+        return 'tie';
+    } else if (playerSelection === 'rock') {
+        if (computerSelection === 'paper') {
+            alert('Computer wins!');
+            return 'cwin';
+        } else {
+            alert('Player wins!');
+            return 'pwin';
+        }
+    } else if (playerSelection ==='paper') {
+        if (computerSelection === 'scissors') {
+            alert('Computer wins!');
+            return 'cwin';
+        } else {
+            alert('Player wins!');
+            return 'pwin';
+        }
+    } else {
+        if (computerSelection === 'rock') {
+            alert('Computer wins!');
+            return 'cwin';
+        } else {
+            alert('Player wins!');
+            return 'pwin';
+        }
+    }
+}
